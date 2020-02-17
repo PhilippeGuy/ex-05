503b15616ee199c0fd8527599ef42fd6ec4c146b
Philippe Guy <philippeguy@hotmail.com>
2020-02-17 16:06:42 -0500

    création de la classe contenu_slider qui génère les images dans le slider

--- mjs/contenu_slider.mjs
+++ mjs/contenu_slider.mjs
@@ -2,5 +2,22 @@
  * class qui génère le contenu du slider de la page à partir d'un fichier .json
  */
 export class contenu_slider{
-    
+    constructor(elmSlides,images){
+        this.elmSlides = elmSlides;
+        this.images = images;
+    }
+
+    /**
+     * génère les images dans le slider
+     */
+    genererImages(){
+        console.log('début génération');
+        console.log(this.imagesJson);
+        const slides = this.elmSlides;
+
+        for(let i=0; i<slides.length; i++){
+            slides[i].style.backgroundImage = "";
+            console.log(i+1);
+        }
+    }
 }
--- mjs/main.mjs
+++ mjs/main.mjs
@@ -1,7 +1,24 @@
 import {anim_entete} from "./anim_entete.mjs";
 import {contenu_slider} from "./contenu_slider.mjs";
 
+//création de l'objet qui contient les images du slider
+let imagesSlider = {
+    "images":[
+        { "nom":"Kiwi", "lien":"images/Kiwi.jpg"},
+        { "nom":"Chameau", "lien":"images/Camel.jpg"},
+        { "nom":"Donkey Kong", "lien":"images/DK.jpg"},
+        { "nom":"Sandwich", "lien":"images/sandwich.png"},
+        { "nom":"Reese", "lien":"images/reese.jpg"}
+    ]
+}
+
 let elmEntete = document.querySelectorAll("header .global>*");
+let elmSlides = document.querySelectorAll(".slide");
+
+
+console.log(elmSlides);
 
 const animEntete = new anim_entete(elmEntete);
+const contenuSlider = new contenu_slider(elmSlides,imagesSlider);
 animEntete.anim();
+contenuSlider.genererImages()
